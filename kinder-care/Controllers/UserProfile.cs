fb591917a35365e5b4ffc4275e463be84bfe938e
OSCAR NARANJO <onaranjo00330@ufide.ac.cr>
2024-12-03 14:37:00 -0600

    ON: Bug Fixes (3/12)

--- kinder-care/Controllers/UserProfile.cs
+++ kinder-care/Controllers/UserProfile.cs
@@ -38,19 +38,17 @@ public class UserProfile : Controller
 
         if (usuario == null) return NotFound();
 
-        if (Nombre != usuario.Nombre || Direccion != usuario.Direccion || CorreoElectronico != usuario.CorreoElectronico)
+        if (Nombre != usuario.Nombre || Direccion != usuario.Direccion ||
+            CorreoElectronico != usuario.CorreoElectronico)
         {
-            if (RoleName == "Padre" || RoleName == "Docente")
-            {
-                var result = await _context.Database.ExecuteSqlRawAsync(
-                    "EXEC GestionarUsuario2 @IdUsuario = {0}, @Cedula = {1}, @Nombre = {2}, @Num_Telefono = {3}, @Direccion = {4}, @CorreoElectronico = {5}, @IdRol = {6}, @Activo = {7}, @Accion = {8}",
-                    usuario.IdUsuario, usuario.Cedula, Nombre, usuario.NumTelefono, Direccion, CorreoElectronico,
-                    usuario.IdRol, usuario.Activo, "ACTUALIZAR");
+            var result = await _context.Database.ExecuteSqlRawAsync(
+                "EXEC GestionarUsuario @IdUsuario = {0}, @Cedula = {1}, @Nombre = {2}, @Num_Telefono = {3}, @Direccion = {4}, @CorreoElectronico = {5}, @IdRol = {6}, @Activo = {7}, @Accion = {8}",
+                usuario.IdUsuario, usuario.Cedula, Nombre, usuario.NumTelefono, Direccion, CorreoElectronico,
+                usuario.IdRol, usuario.Activo, "ACTUALIZAR");
 
-                if (result == 0) return NotFound();
+            if (result == 0) return NotFound();
 
-                return RedirectToAction("Index", "UserProfile", new { id = id });
-            }
+            return RedirectToAction("Index", "UserProfile", new { id = id });
         }
 
         return RedirectToAction("Index", "UserProfile", new { id = id });
