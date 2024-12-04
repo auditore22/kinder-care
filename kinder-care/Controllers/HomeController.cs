a0fc7fe6fd2d8e24f6d5e510aad8192f30d68fbc
Juan D <Jdsang18@gmail.com>
2024-12-03 21:20:53 -0600

    Test Dashboard

--- kinder-care/Controllers/HomeController.cs
+++ kinder-care/Controllers/HomeController.cs
@@ -3,20 +3,21 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using kinder_care.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 namespace kinder_care.Controllers;
 
 [Authorize]
 public class HomeController(ILogger<HomeController> logger) : Controller
 {
+    private readonly KinderCareContext _context;
     private readonly ILogger<HomeController> _logger = logger;
 
     public IActionResult Index()
     {
         var currentUserId = User.Claims.Where(c => c.Type == ClaimTypes.NameIdentifier).Select(c => c.Value).SingleOrDefault();
         int parsedCurrentUserId = int.TryParse(currentUserId, out int result) ? result : -1; // Convierte a entero o usa un valor no válido (-1) si falla
-        
-        ViewBag.UserId = parsedCurrentUserId;
+
         return View();
     }
     
